923c200c24ef1df90e432586a7a49808003f4620
Igorpignata12 <igorpignata@gmail.com>
2026-09-16 10:56:24 -0300

    first commit

---
+++ src/main/java/dev/ipignata/CadastroDeDevs/CadastroDeDevsApplication.java
@@ -0,0 +1,13 @@
+package dev.ipignata.CadastroDeDevs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeDevsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeDevsApplication.class, args);
+	}
+
+}
