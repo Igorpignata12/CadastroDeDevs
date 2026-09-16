0447d4ff443080b8c430323e1fc9bc0b4a7d89ab
Igorpignata12 <igorpignata@gmail.com>
2026-09-16 11:28:39 -0300

    classe controller criada

--- src/main/java/dev/ipignata/CadastroDeDevs/Controller.java
+++ src/main/java/dev/ipignata/CadastroDeDevs/Controller.java
@@ -1,7 +1,20 @@
 package dev.ipignata.CadastroDeDevs;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Igor Pignata
  */
- 
+
+@RestController
+@RequestMapping
 public class Controller {
+
+    @GetMapping("/teste")
+    public String teste(){
+        return "Teste";
+    }
+
 }
